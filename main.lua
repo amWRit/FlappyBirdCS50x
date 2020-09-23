7672e07137e84a885c76eb773336d93062811f35
amWRit <derasrocks@gmail.com>
2020-09-23 17:13:43 +0545

    single and pipe pair

--- main.lua
+++ main.lua
@@ -2,6 +2,8 @@ push = require 'push'
 Class = require 'class'
 
 require 'Bird'
+require 'Pipe'
+require 'PipePair'
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
@@ -20,6 +22,11 @@ local GROUND_SCROLL_SPEED = 60
 local BACKGROUND_LOOPING_POINT = 413
 
 local bird = Bird()
+-- local pipes = {}
+local pipePairs = {}
+local spawnTimer = 0
+
+local lastY = -PIPE_HEIGHT + math.random(80) + 20
 
 function love.load()
 	love.graphics.setDefaultFilter('nearest', 'nearest')
@@ -55,6 +62,30 @@ function love.update(dt)
 	backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
 	groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
 	
+	spawnTimer = spawnTimer + dt
+
+	-- time in seconds
+	if spawnTimer > 2 then
+		local y = math.max(-PIPE_HEIGHT + 10, 
+			math.min(lastY + math.random(-20,20),
+				VIRTUAL_HEIGHT - 90 - PIPE_HEIGHT))
+		table.insert(pipePairs, PipePair(y))
+		spawnTimer = 0
+	end
+
+	-- key,value pairs
+	for k, pair in pairs(pipePairs) do
+		pair:update(dt)
+
+
+	end
+
+	for k, pair in pairs(pipePairs) do
+		if pair.remove then
+			table.remove(pipePairs, k)
+		end
+	end
+
 	bird:update(dt)
 
 	love.keyboard.keysPressed = {}
@@ -65,6 +96,9 @@ function love.draw()
 	push:start()
 	love.graphics.draw(background, -backgroundScroll, 0)
 
+	for k, pair in pairs(pipePairs) do
+		pair:render()
+	end
 	--love.graphics.draw(drawable, x, y, r, sx, sy, ox, oy, kx, ky)
 	love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT-16)
 	-- similar to push:apply('finish')
